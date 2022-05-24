2bd852dc60c0aaf5c93ca8fc98e3a1dc94793ab6
relaxnow <boy@relaxnow.nl>
2022-05-24 17:40:16 +0200

    7 files modified, 6 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialExampleModule } from '../material.module';
 
 import { AppComponent } from './app.component';
+import { StepperEditableExample } from './stepper-editable-example/stepper-editable-example';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
+    MaterialExampleModule,
     BrowserModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ]),
-    BrowserAnimationsModule
+    RouterModule.forRoot([{ path: '', component: StepperEditableExample }]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    StepperEditableExample,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
